773550577dbfd888bf8070e06beaa4c57ccfad20
HTML-Experts <mq30994@gmail.com>
2023-08-26 05:52:02 -1200

    adding new animatons

--- src/components/home/HomeSections/HomeSec2.jsx
+++ src/components/home/HomeSections/HomeSec2.jsx
@@ -2,27 +2,74 @@ import React from "react";
 import "../HomeStyle.scss";
 import CountUp from "./CountUp";
 import { section2 } from "../../../siteContent/home/HomeCon";
+import { motion } from "framer-motion";
+
+const homeSec2Variants = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  animate: {
+    opacity: 1,
+    y: 1,
+    transition: {
+      delay: 0.05,
+      duration: 0.3,
+    },
+  },
+};
+
+const homeSec21Variants = {
+  initial: {
+    opacity: 0,
+    x: -30,
+  },
+  animate: (index) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.3 * index,
+      duration: 0.5,
+    },
+  }),
+};
 
 const HomeSec2 = () => {
   return (
     <div className="HomeSec2">
-      <CountUp/>
+      <CountUp />
       <div className="main">
         <div className="content">
-          <h1>
+          <motion.h1
+            variants={homeSec2Variants}
+            initial="initial"
+            whileInView="animate"
+          >
             Our Users <span>Love Learning </span> With Our <br /> App
-          </h1>
-          <p>
+          </motion.h1>
+          <motion.p
+            variants={homeSec2Variants}
+            initial="initial"
+            whileInView="animate"
+          >
             Consectetur adipisicing elit, sed do eiusmod tempor incididunt.{" "}
             <br />
             Consectetur sed do eiusmod tempor incididunt adipisicing eiusmod
             tempor incididunt elit.
-          </p>
+          </motion.p>
 
           <div className="inenerContent">
             <div className="contentMain">
-              {section2.map((sec) => (
-                <div className="cn_outer" key={sec.title}>
+              {section2.map((sec, index) => (
+                <motion.div
+                  className="cn_outer"
+                  key={sec.title}
+                  variants={homeSec21Variants}
+                  initial="initial"
+                  whileInView="animate"
+                  custom={index}
+                  viewport={{ once: true }}
+                >
                   <div className="sec2Img">
                     <img src={sec.Icon} alt="" />
                   </div>
@@ -30,7 +77,7 @@ const HomeSec2 = () => {
                     <h1>{sec.title}</h1>
                     <p>{sec.details}</p>
                   </div>
-                </div>
+                </motion.div>
               ))}
             </div>
           </div>
